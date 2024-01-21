f96983bc75d359021f5973802a46d831c71de8af
Pasika Sriwanna <pasika.swn@gmail.com>
2024-01-22 02:57:43 +0700

    none

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,22 @@
-// @ts-check
-
-
-// (in terminal) yarn add prop-types -D
-import PropTypes from "prop-types";
-
-// import { useState } from "react";
-import React, { useState } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-// this code below is "JS Doc" --> insert data (type)
-/**
- * 
- * @type {React.FC<{ name: string }>}
- * @returns {React.ReactNode}
- */
 
-
-
-
-
-// props is an immutable obj
-//---------------------------------------------------
-// const SelfIntroduction = (props) => {
-//   return <h1>"My name is {props.name}."</h1>;
-// };
-//default -> use when undefied only
 const SelfIntroduction = ({ name = "unknown", dateOfBirth, hobbies = [] }) => {
   return (
     <div>
       <h1>"My name is {name}."</h1>
-      {/* <h2>I was born in {dateOfBirth}</h2> */}
-
-      {/* write a short-if */}
       {dateOfBirth ? <h2>I was born in {dateOfBirth}</h2> : null}
-      {/* --------------------- */}
-
       {hobbies.length > 0 ? (
-        //{/* อยากได้ JSX (no string) -> use .map() instead of .join() */}
         <div>
-          {/* <h2>My hobbies are: {hobbies.join(", ")}</h2> */}
           <h2>My hobbies are: </h2>
           {hobbies.map((item, index) => (
-            //{/* Must have KEY!!! */}
             <div key={item}>
               {index + 1}. {item}
             </div>
           ))}
-          {/* {hobbies.map()} */}
         </div>
       ) : null}
     </div>
@@ -70,8 +38,6 @@ function App() {
       />
 
       <Test>
-        <div>test2</div>
-        {/* 123 -> string not number */}
         123
       </Test>
     </div>
